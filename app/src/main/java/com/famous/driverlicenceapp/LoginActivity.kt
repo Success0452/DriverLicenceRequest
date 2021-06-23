09681b5c6299091b2e7e18f7fa2fc1431ca27f8f
Success0452 <olatundesuccess54@gmail.com>
2021-06-23 20:31:56 +0100

    Update

---
+++ app/src/main/java/com/famous/driverlicenceapp/LoginActivity.kt
@@ -0,0 +1,114 @@
+package com.famous.driverlicenceapp
+
+import android.app.ProgressDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import com.famous.driverlicenceapp.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    //ActionBar
+    private lateinit var actionBar: ActionBar
+
+    //ProgressDialog
+    private lateinit var progressDialog : ProgressDialog
+
+    //FirebaseAuth
+    private lateinit var firebaseAuth : FirebaseAuth
+    private var email = ""
+    private var password = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Configure ActionBar
+        actionBar = supportActionBar!!
+        actionBar.title = "Login"
+
+        //configure ProgressDialog
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setMessage("Logging in")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        //init firebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        //handle click, open sign up
+        binding.loginBtn.setOnClickListener{
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+        //handle click, begin login
+        binding.signB.setOnClickListener{
+            //before login, validate data
+            validateData()
+        }
+
+
+    }
+
+    private fun validateData() {
+        //get data
+        email = binding.emailLogin.text.toString().trim()
+        password = binding.passwordLogin.text.toString().trim()
+
+        //validate data
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            //invalid email format
+            binding.emailLogin.error = "Invalid Email Format"
+        }else if (TextUtils.isEmpty(password)){
+            binding.passwordLogin.error = "Please Enter Password"
+        } else {
+            //data is validated, begin login
+            firebaseLogin()
+        }
+
+
+    }
+
+    private fun firebaseLogin() {
+        //show progress
+        progressDialog.show()
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                //login success
+                progressDialog.dismiss()
+                //get User info
+                val firebaseUser = firebaseAuth.currentUser
+                val email = firebaseUser!!.email
+                Toast.makeText(this, "Login as $email", Toast.LENGTH_LONG).show()
+                //open main activity
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+
+            }
+            .addOnFailureListener{ e->
+                //login failure
+                progressDialog.dismiss()
+                Toast.makeText(this, "Login failed due to ${e.message}", Toast.LENGTH_LONG).show()
+            }
+    }
+
+    private fun checkUser() {
+        //if user is logged in go to profile activity
+        //get Current User
+
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser != null){
+            //user is already logged in
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/famous/driverlicenceapp/SignUpActivity.kt
@@ -0,0 +1,107 @@
+package com.famous.driverlicenceapp
+
+import android.app.ProgressDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import com.famous.driverlicenceapp.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+
+    //ActionBar
+    private lateinit var actionBar: ActionBar
+
+    //ProgressDialog
+    private lateinit var progressDialog : ProgressDialog
+
+    //FirebaseAuth
+    private lateinit var firebaseAuth : FirebaseAuth
+    private var email = ""
+    private var password = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//configure ActionBar
+        actionBar = supportActionBar!!
+        actionBar.title = "SignUp"
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        actionBar.setDisplayShowHomeEnabled(true)
+
+        //configure ProgressDialog
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setMessage("Creating Account")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        //init firebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        //handle click, begin signUp
+        binding.signupBtn.setOnClickListener{
+            //validate date
+            validateData()
+        }
+    }
+
+    private fun validateData() {
+        //get data
+        email = binding.emailSignup.text.toString().trim()
+        password = binding.passwordSignup.toString().trim()
+
+        //validate data
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            //invalid email format
+            binding.emailSignup.error = "Invalid Email Format"
+        }else if (TextUtils.isEmpty(password)) {
+            //password isn't entered
+            binding.passwordSignup.error = "password isn't entered"
+
+        }else if (password.length < 6){
+            //password length is less than 6
+            binding.passwordSignup.error = "Password must ave atleast 6 character"
+        }else {
+            //data is valid, continue sign up
+            firebaseSignUp()
+        }
+    }
+
+    private fun firebaseSignUp() {
+        //show progress
+        progressDialog.show()
+
+        //create account
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                //signUp success
+                progressDialog.dismiss()
+                //get currentUser
+                val firebaseUser = firebaseAuth.currentUser
+                val email = firebaseUser!!.email
+                Toast.makeText(this, "Account Created with email ${email}", Toast.LENGTH_LONG).show()
+
+                //open mainActivity
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+            .addOnFailureListener{ e->
+                //signUp failure
+                progressDialog.dismiss()
+                Toast.makeText(this, "SignUp failed due to ${e.message}", Toast.LENGTH_LONG).show()
+            }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed() //go back to previous activity, when back button of action button is clicked
+        return super.onSupportNavigateUp()
+    }
+
+}
